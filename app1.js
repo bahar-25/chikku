54665ecd371fa70c7d9289ea1c7a103049363fa4
bahar-25 <shaiksbaharunnisa@gmail.com>
2026-04-23 18:52:17 +0530

    Add files via upload

--- app1.js
+++ app1.js
@@ -1,6 +1,3 @@
-function resize(){
-    let height=document.getElementById('value1').value
-    let weight=document.getElementById('value2').value
-    document.getElementById('image').style.height=`${height}px`
-    document.getElementById('image').style.height=`${weight}px`
+ function dog() {
+    document.getElementById("Image").innerHTML += `<img src="https://place.dog/300/200?${Date.now()}">`;
 }
