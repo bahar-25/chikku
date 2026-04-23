5a57a8acf63c9eec6a435e19267d16f99db37efe
bahar-25 <shaiksbaharunnisa@gmail.com>
2026-04-23 18:49:22 +0530

    Add files via upload

---
+++ app1.js
@@ -0,0 +1,6 @@
+function resize(){
+    let height=document.getElementById('value1').value
+    let weight=document.getElementById('value2').value
+    document.getElementById('image').style.height=`${height}px`
+    document.getElementById('image').style.height=`${weight}px`
+}
