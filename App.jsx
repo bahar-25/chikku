4ee10bff1becc57d7ae77caaae7358e0ea5fcfb8
bahar-25 <shaiksbaharunnisa@gmail.com>
2026-04-24 13:46:41 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,9 @@
+import Foodcart from "./Foodcart" 
+function App(){
+  return (
+    <>
+    <Foodcart name="Briyani" price="150" image="C:\Users\HP\Downloads\rest(1).jpg" />
+    </>
+  )
+}
+export default App
