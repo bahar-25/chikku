3cb860594735f855957872460de0cc9d1cbedda7
bahar-25 <shaiksbaharunnisa@gmail.com>
2026-04-25 21:47:22 +0530

    Add files via upload

---
+++ Header1.jsx
@@ -0,0 +1,18 @@
+function Header1(){
+    return (
+        <>
+        <div style={{backgroundColor:"whitesmoke"}}>
+        <div style={{display:"1px soild black",justifyContent:"center"}}>
+            <img src="C:\Users\HP\Desktop\reactfolder\public\favicon.svg" alt="" />
+            <nav style={{display:"felx",gap:"20px"}}>
+            <a href="#"> HOME</a>
+            <a href="#"> PRODUCT</a>
+            <a href="#"> ABOUT</a>
+            <a href="#"> TREMS</a>
+          </nav>
+        </div>
+        </div>
+        </>
+    )
+}
+export default Header1
