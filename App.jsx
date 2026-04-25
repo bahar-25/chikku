8dd1dbb8614bde4be908b7e239c16529a96c485a
bahar-25 <shaiksbaharunnisa@gmail.com>
2026-04-25 21:48:22 +0530

    Add files via upload

--- App.jsx
+++ App.jsx
@@ -1,9 +1,40 @@
-import Foodcart from "./Foodcart" 
-function App(){
+import { useState,useEffect } from "react"
+import Header1 from "./Header1"
+function App() {
+
+  const [product, setproduct] = useState([])
+
+useEffect(() => {
+  
+fetch('https://fakestoreapi.com/products')
+  .then(response => response.json())
+  .then(data => setproduct(data));
+ 
+}, [])
+
+
+
+  //fetch the data
+  
+
   return (
     <>
-    <Foodcart name="Briyani" price="150" image="C:\Users\HP\Downloads\rest(1).jpg" />
+    <Header1/>
+    <div style={{display:'flex',flexWrap:"wrap", gap:"20px"}} >
+      {
+      product.map((p)=>{
+        return <div key={p.id} >
+          <img height={200} width={200} src={p.image} alt="" />
+          <p>{p.title.substring(0,10)}</p>
+          <p>{p.price}</p>
+          <p>{p.description.substring(0,10)}</p>
+        </div>
+      })
+    }
+    </div>
+    
     </>
   )
 }
+
 export default App
